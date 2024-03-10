e8b9e197ebd7d0f48ca7a4b30eb28e74bfb3b80e
neutronplsr <chiariniolivia@gmail.com>
2024-03-10 11:28:17 -0400

    automatic push from update

--- programs/rofi/rofi.nix
+++ programs/rofi/rofi.nix
@@ -1,22 +1,42 @@
 { config, pkgs, ...}: {
-	programs.rofi = {
-		enable = true;
-		configPath = "./.";
-		theme = "./catppuccin-mocha.rasi";
-		extraConfig = {
-		    modi= "run,drun,window";
-		    icon-theme= "Oranchelo";
-		    show-icons= true;
-		    terminal= "kitty";
-		    drun-display-format= "{icon} {name}";
-		    location= 0;
-		    disable-history= false;
-		    hide-scrollbar= true;
-		    display-drun= "   Apps ";
-		    display-run= "   Run ";
-		    display-window= " 﩯  Window";
-		    display-Network= " 󰤨  Network";
-		    sidebar-mode= true;
-		};
-	};
+	# programs.rofi = {
+	# 	enable = true;
+	# 	configPath = "./.";
+	# 	theme = "./catppuccin-mocha.rasi";
+	# 	extraConfig = {
+	# 	    modi= "run,drun,window";
+	# 	    icon-theme= "Oranchelo";
+	# 	    show-icons= true;
+	# 	    terminal= "kitty";
+	# 	    drun-display-format= "{icon} {name}";
+	# 	    location= 0;
+	# 	    disable-history= false;
+	# 	    hide-scrollbar= true;
+	# 	    display-drun= "   Apps ";
+	# 	    display-run= "   Run ";
+	# 	    display-window= " 﩯  Window";
+	# 	    display-Network= " 󰤨  Network";
+	# 	    sidebar-mode= true;
+	# 	};
+	# };
+	home.file.".config/rofi/config.rasi".text = ''
+		configuration{
+		    modi: "run,drun,window";
+		    icon-theme: "Oranchelo";
+		    show-icons: true;
+		    terminal: "alacritty";
+		    drun-display-format: "{icon} {name}";
+		    location: 0;
+		    disable-history: false;
+		    hide-scrollbar: true;
+		    display-drun: "   Apps ";
+		    display-run: "   Run ";
+		    display-window: " 﩯  Window";
+		    display-Network: " 󰤨  Network";
+		    sidebar-mode: true;
+		}
+		
+		@theme "catppuccin-mocha"
+	''
+	
 }
