2feb8ee9e0efe64e1aa5851e5f3ef1e51b39199f
neutronplsr <chiariniolivia@gmail.com>
2024-04-06 14:57:00 -0400

    automatic commmit from update

--- home.nix
+++ home.nix
@@ -83,6 +83,17 @@
 	# session variables
 	home.sessionVariables = {
 		EDITOR = "micro";
+		XDG_SESSION_TYPE =  "wayland";
+		GDK_BACKEND =  "wayland,x11";
+		XDG_SCREENSHOTS_DIR = "$HOME/Pictures/Screenshots";
+		XDG_CACHE_HOME  = "$HOME/.cache";
+		XDG_CONFIG_HOME = "$HOME/.config";
+		XDG_DATA_HOME   = "$HOME/.local/share";
+		XDG_STATE_HOME  = "$HOME/.local/state";
+		XDG_BIN_HOME    = "$HOME/.local/bin";
+    	PATH = [ 
+      		"${XDG_BIN_HOME}"
+    	];
 	};
 
 	# git config
