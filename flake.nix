19b83b66a9f6bacd4e350e34b6bb5731c7e1724d
neutronplsr <chiariniolivia@gmail.com>
2024-04-06 21:21:47 -0400

    automatic commmit from update

--- flake.nix
+++ flake.nix
@@ -57,7 +57,7 @@
 			};
 		};
 		overlays = {
-		  unstable-packages = = final: _prev: {
+		  unstable-packages = final: _prev: {
 		      unstable = import inputs.nixpkgs-unstable {
 		        system = final.system;
 		        config.allowUnfree = true;
