a92dde4725fa552f113d741f95d7a29038fca1e9
neutronplsr <chiariniolivia@gmail.com>
2024-04-06 20:48:59 -0400

    automatic commmit from update

--- wms/hyprland/hyprlock.nix
+++ wms/hyprland/hyprlock.nix
@@ -3,5 +3,23 @@
 	programs.hyprlock = {
 		enable = true;
 	};
-
+	services.swayidle = {
+	    enable = true;
+	    timeouts = [
+	      {
+	        timeout = 115;
+	        command = "${pkgs.libnotify}/bin/notify-send 'Locking in 5 seconds' -t 5000";
+	      }
+	      {
+	        timeout = 120;
+	        command = "${pkgs.hyprlock}/bin/hyprlock";
+	      }
+	    ];
+	    events = [
+	      {
+	        event = "before-sleep";
+	        command = "${pkgs.hyprlock}/bin/hyprlock";
+	      }
+	    ];
+	  };
 }
