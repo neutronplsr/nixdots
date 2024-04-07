b5e3de4b003a8500933f0cdc9f902c426273f9ae
neutronplsr <chiariniolivia@gmail.com>
2024-04-06 21:43:35 -0400

    automatic commmit from update

--- wms/hyprland/hyprlock.nix
+++ wms/hyprland/hyprlock.nix
@@ -75,13 +75,13 @@
 	      }
 	      {
 	        timeout = 10;
-	        command = "${pkgs.hyprlock}/bin/hyprlock";
+	        command = "programs.hyprlock";
 	      }
 	    ];
 	    events = [
 	      {
 	        event = "before-sleep";
-	        command = "${pkgs.hyprlock}/bin/hyprlock";
+	        command = "programs.hyprlock";
 	      }
 	    ];
 	  };
