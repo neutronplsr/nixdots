387e0fa59124358fc984da12a1bc1e731c7dccb4
neutronplsr <chiariniolivia@gmail.com>
2024-04-11 08:10:05 -0400

    automatic commmit from update

--- wms/hyprland/hyprlock.nix
+++ wms/hyprland/hyprlock.nix
@@ -75,7 +75,7 @@
 	      }
 	      {
 	        timeout = 10;
-	        command = "hyprlock";
+	        command = "${pkgs.libnotify}/bin/notify-send 'Locked' -t 5000";
 	      }
 	    ];
 	    events = [
