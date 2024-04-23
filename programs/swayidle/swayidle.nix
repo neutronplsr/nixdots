5e0f9080f4c83971d3e1e897b51032e3bdb07640
neutronplsr <chiariniolivia@gmail.com>
2024-04-23 15:47:47 -0400

    automatic commmit from update

--- programs/swayidle/swayidle.nix
+++ programs/swayidle/swayidle.nix
@@ -5,9 +5,7 @@
 
 	services.swayidle = 
 	let 
-	lockCommand = "${pkgs.swaylock}/bin/swaylock   \
-					--clock\
-					--indicator ";
+	lockCommand = "${pkgs.swaylock}/bin/swaylock   swaylock --clock  --indicator 	--indicator-radius 100--indicator-thickness 7 --effect-blur 7x5 	--effect-vignette 0.5:0.5 	--ring-color bb00cc 	--key-hl-color 880033 	--line-color 00000000 	--inside-color 00000088 --separator-color 00000000 --fade-in 0.2";
 	in {
 	    enable = true;
 	    timeouts = [
